4169d4cae016d57c7709a99236402516c6e5ad6b
HoanThuat <hoanthuatenji@gmail.com>
2020-03-18 16:08:07 +0700

    Learn react props.children Bai 8

--- public/xuly.js
+++ public/xuly.js
@@ -1,12 +1,19 @@
 class KhoaPham extends React.Component{
   render(){
-    return <h1 className = "mau-vang"> Hello, {this.props.ten}</h1>
+    return <div>
+      <h1 className = "mau-vang"> Hello, {this.props.ten}</h1>
+      <p>{this.props.children}</p>
+    </div>
+      
   };
 };
 
 class NewH2 extends React.Component {
   render() {
-  return <h2> Hello, {this.props.ten}</h2>
+  return <div>
+    <h2> Hello, {this.props.ten}</h2>
+    <p>{this.props.children}</p>
+  </div>
   };
 };
 
@@ -17,10 +24,11 @@ class NewImg extends React.Component {
 }
 
 
+
   
 const Div = <div>
-    <KhoaPham ten="Nguyen Hoan Thuat"/>
-    <NewH2 ten="Nguyen Hoan Bao"/>
+    <KhoaPham ten="Nguyen Hoan Thuat">Khoa hoc React</KhoaPham>
+    <NewH2 ten="Nguyen Hoan Bao">Khoa hoc NodeJs</NewH2>
     <NewImg />
   </div>
 
